c7ded827087ad86f9309eebc6896fa4f4eeacfe4
Aravindkumar <aravindkumar.s@bugasura.io>
2026-01-18 10:39:28 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();   // remove all session variables
-session_destroy(); // destroy the session
-
-header("Location: login.php"); // redirect to login page
-exit();
-?>
